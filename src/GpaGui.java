2c9d3a325c41689dfbde4e9d121103918239c736
aagavin <aferna95@my.centennialcollege.ca>
2015-11-08 15:59:46 -0500

    more gui

--- src/GpaGui.java
+++ src/GpaGui.java
@@ -18,10 +18,6 @@ public class GpaGui extends JFrame implements ActionListener {
 	private JLabel _gpaTotalTxt;
 	private JButton _exitBtn;
 	private JScrollPane _sceoolPane;
-	private JLabel _nrLbl;
-	private JLabel _courseCodeLbl;
-	private JLabel _creditHrsLbl;
-	private JLabel _qradeLbl;
 
 	private JPanel _rowPanel;
 	private JLabel _titleLbl;
@@ -38,7 +34,7 @@ public class GpaGui extends JFrame implements ActionListener {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setIconImage(new ImageIcon(getClass().getResource("/images/edit-text.png")).getImage());
 		setMinimumSize(new Dimension(650,300));
-		setSize(700,600);
+		setSize(700,500);
 		setLocation(400,100);
 		setVisible(true);
 	}
@@ -50,9 +46,29 @@ public class GpaGui extends JFrame implements ActionListener {
 
 	private void _addRows(int numRows){
 
-		this._grid.setRows(numRows);
+		this._rowPanel.removeAll();
+		this._rowPanel.revalidate();
+		this._rowPanel.validate();
+
+		this._rowPanel.add(new JLabel("Nr:"));
+		this._rowPanel.add(new JLabel("Course Code:"));
+		this._rowPanel.add(new JLabel("Credit Hours:"));
+		this._rowPanel.add(new JLabel("Grade:"));
+
+
 		for (int i=0; i<numRows; i++){
-			this._rowPanel.add(new JLabel(i+". "));
+			this._rowPanel.add(new JLabel((i+1)+". "));
+			this._rowPanel.add(new JTextField());
+			this._rowPanel.add(new JComboBox<>(new Integer[]{1,2,3,4,5}));
+			this._rowPanel.add(new JComboBox<>(new String[]{"","A+ 90-100%","A 80-89%","B+ 75-79%","B 75-79%","C+ 65-69%","C 60-64%","D+ 55-59%","D 50-54%","F 0-49%"}));
+		}
+		this._rowPanel.revalidate();
+		this._rowPanel.validate();
+	}
+
+	private void _calculateBtnAction() {
+		for (Component compnent : this._rowPanel.getComponents()){
+			System.out.print(compnent.getName());
 		}
 	}
 
@@ -66,17 +82,19 @@ public class GpaGui extends JFrame implements ActionListener {
 			try {
 				int selected = Integer.parseInt(this._numCoursesCbx.getSelectedItem().toString());
 				this._addRows(selected);
-				System.out.printf("%n%d", selected);
 			}catch (NumberFormatException e){e.printStackTrace();}
-
-
+		}
+		if (actionEvent.getSource()==this._calculateBtn){
+//			this._calculateBtnAction();
+			JOptionPane.showMessageDialog(null,"LKSD");
 		}
 
 	}
 
 	private void createUIComponents() {
-		this._grid = new GridLayout();
-		this._numCoursesCbx = new JComboBox<String>(new String[]{"","1","2","3","4","5","6","7","8","9","10"});
+		this._grid = new GridLayout(0,4);
+		this._numCoursesCbx = new JComboBox<>(new String[]{"","1","2","3","4","5","6","7","8","9","10"});
 		this._rowPanel=new JPanel(this._grid);
+		this._rowPanel.setMinimumSize(new Dimension(0,0));
 	}
 }
